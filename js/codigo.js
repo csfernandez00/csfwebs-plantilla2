68857f168b501c19a199ad622b67ae90c7310fc6
csfernandez00 <csfernandez00@gmail.com>
2022-06-28 18:08:31 -0300

    close nav btn fixed

--- js/codigo.js
+++ js/codigo.js
@@ -2,20 +2,22 @@ const menuBtn = document.querySelector("#btn-menu");
 const mobileNav = document.querySelector(".mobile-nav");
 const mobileNavBtn1 = document.querySelector(".mobile-nav-a1");
 const mobileNavBtn2 = document.querySelector(".mobile-nav-a2");
+const closeNavBtn = document.getElementById("close-mobile-nav");
 
 
 menuBtn.addEventListener("click", () => {
-    menuBtn.classList.toggle("fa-times");
-    mobileNav.classList.toggle("active")
+    mobileNav.classList.toggle("active");
 })
 
 
 mobileNavBtn1.addEventListener("click", () => {
-    menuBtn.classList.remove("fa-times");
     mobileNav.classList.remove("active")
 })
 
 mobileNavBtn2.addEventListener("click", () => {
-    menuBtn.classList.remove("fa-times");
     mobileNav.classList.remove("active")
+})
+
+closeNavBtn.addEventListener("click", ()=>{
+    mobileNav.classList.remove("active");
 })
